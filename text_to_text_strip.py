806909962f17257acd189be9da56b7ac187b25b0
Adrien Gomez <57804480+AdrienGomez@users.noreply.github.com>
2024-08-01 09:15:28 +0200

    Add files via upload

---
+++ text_to_text_strip.py
@@ -0,0 +1,31 @@
+import bpy
+### This script convert sentences in a raw text to text strip. length is define by word count ### 
+
+#text to convert to text strip in sequence editor
+thetext = "Three Rings for the Elven-kings under the sky. Seven for the Dwarf-lords in their halls of stone. Nine for Mortal Men doomed to die. One for the Dark Lord on his dark throne. In the Land of Mordor where the Shadows lie. One Ring to rule them all. One Ring to find them. One Ring to bring them all and in the darkness bind them.In the Land of Mordor where the Shadows lie."
+
+# define average number of frame per word
+framerate = bpy.data.scenes['Scene'].render.fps
+wordspermin = (framerate*60)/150
+
+#convert text to a list of sentences
+thesentences = thetext.split(".")
+
+#unse the Frame start as a starting point for text strips)
+stripstart= bpy.data.scenes["Scene"].frame_start
+
+for sentences in thesentences:
+    # define the strip length
+    striplength = wordspermin * len(sentences.split())
+    
+    #add strips
+    lestrip=bpy.data.scenes['Scene'].sequence_editor.sequences.new_effect(name=sentences, type='TEXT', channel=5, frame_start = int(stripstart), frame_end = int(stripstart+striplength))
+    
+    #define text for the strip
+    lestrip.text = sentences
+    
+    #define the next strip frame start
+    stripstart += striplength
+    
+    
+#(name, type, channel, frame_start, frame_end=0, seq1=None, seq2=None, seq3=None)    
